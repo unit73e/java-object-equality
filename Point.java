1ad6bd13e96f6b0275b747e388b63ed582dfb245
Emanuel Couto <ext.emanuel.couto@inseinc.com>
2017-07-17 13:39:29 +0100

    Fix wrong hash code
    The hash code was not redefined and for that reason it was
    auto-generated by the compiler. Equal objects must always return equal
    hash codes but with an auto-generated hash code that is not the case.

--- Point.java
+++ Point.java
@@ -1,5 +1,7 @@
 import java.util.Set;
 import java.util.HashSet;
+import java.util.Objects;
+import java.util.Iterator;
 
 public class Point {
 	private int x;
@@ -38,6 +40,11 @@ public class Point {
 	}
 
 	@Override
+	public int hashCode() {
+		return Objects.hash(this.getX(), this.getY());
+	}
+
+	@Override
 	public String toString() {
 		return String.format("Point(%d,%d)", this.getX(), this.getY());
 	}
@@ -50,23 +57,29 @@ public class Point {
 		System.out.println("P2 = " + p2);
 		System.out.println();
 
-		// Should be true since P1 equals P2A
-		Object p2a = p2;
-		System.out.println("P2A = (Object) " + p2);
-		System.out.println("P1 equals P2A = " + p1.equals(p2a));
-		System.out.println();
-
 		Set<Point> s = new HashSet<>();
 		s.add(p1);
 		System.out.println("S = " + s);
 		// Should be true since P1 was added to S
 		System.out.println("S contains P1 = " + s.contains(p1));
 		// Should be true since P1 equals P2
-		//
-		// However returns false since `hashcode` is not implemented
-		// according to `equals`
 		System.out.println("S contains P2 = " + s.contains(p2));
 		System.out.println();
 
+		// Should show Point(2,2)
+		p1.setX(2);
+		System.out.println("P1.X = 2 ");
+		System.out.println("S = " + s);
+		// Should return true
+		//
+		// However returns false because P1 is in the wrong hash bucket
+		// and will never be found for that reason.
+		//
+		// Never use mutable fields for object equality since it will
+		// cause problems when used in collections.
+		System.out.println("S contains P1 = " + s.contains(p1));
+		System.out.println("S contains " + p1 + " = " +
+				s.contains(new Point(2,2)));
+		System.out.println();
 	}
 }
