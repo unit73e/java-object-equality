e2cb1927c61f9b0a69e72da26340ca0f2276c46b
Emanuel Couto <ext.emanuel.couto@inseinc.com>
2017-07-17 17:25:05 +0100

    Fix mutable fields
    The implementation of `equals` and `hashCode` should never be based on
    mutable fields because unexpected behavior will occur if these objects
    are inserted into collections, such as an `HashSet`.

--- Point.java
+++ Point.java
@@ -4,8 +4,8 @@ import java.util.Objects;
 import java.util.Iterator;
 
 public class Point {
-	private int x;
-	private int y;
+	final private int x;
+	final private int y;
 
 	public Point(int x, int y) {
 		this.x = x;
@@ -20,12 +20,8 @@ public class Point {
 		return y;
 	}
 
-	public void setX(int x) {
-		this.x = x;
-	}
-
-	public void setY(int y) {
-		this.y = y;
+	public Point move(int x, int y) {
+		return new Point(this.getX() + x, this.getY() + y);
 	}
 
 	@Override
@@ -51,35 +47,22 @@ public class Point {
 
 	public static void main(String[] args) {
 		Point p1 = new Point(1, 2);
-		Point p2 = new Point(1, 2);
+		Point p2 = new Point(2, 2);
 
 		System.out.println("P1 = " + p1);
 		System.out.println("P2 = " + p2);
 		System.out.println();
 
-		Set<Point> s = new HashSet<>();
-		s.add(p1);
-		System.out.println("S = " + s);
-		// Should be true since P1 was added to S
-		System.out.println("S contains P1 = " + s.contains(p1));
-		// Should be true since P1 equals P2
-		System.out.println("S contains P2 = " + s.contains(p2));
+		// Should return true since (2,3) == (2,3)
+		Point p3 = p1.move(1, 0);
+		System.out.println("P3 = P1 move (1,0)");
+		System.out.println("P2 equals P3 = " + p2.equals(p3));
 		System.out.println();
 
-		// Should show Point(2,2)
-		p1.setX(2);
-		System.out.println("P1.X = 2 ");
+		Set<Point> s = new HashSet<>();
+		s.add(new Point(2,2));
 		System.out.println("S = " + s);
-		// Should return true
-		//
-		// However returns false because P1 is in the wrong hash bucket
-		// and will never be found for that reason.
-		//
-		// Never use mutable fields for object equality since it will
-		// cause problems when used in collections.
-		System.out.println("S contains P1 = " + s.contains(p1));
-		System.out.println("S contains " + p1 + " = " +
-				s.contains(new Point(2,2)));
-		System.out.println();
+		System.out.println("S contains P2 = " + s.contains(p2));
+		System.out.println("S contains P3 = " + s.contains(p3));
 	}
 }
